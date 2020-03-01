bb02acfcee86194885c76fd3179c5845af46da98
pwalton134 <35519675+pwalton134@users.noreply.github.com>
2020-03-01 11:28:56 +0800

    Delete run-openttd.sh

--- run-openttd.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d --name openttd-jgr -e LOCAL_USER_ID=`id -u $USER` -p 3979:3979/tcp -p 3979:3979/udp --restart=always --log-opt max-size=50m -v ~/openttd-data:/config:rw pwalton134/openttd-jgr:latest

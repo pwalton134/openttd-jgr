e0503f9d9a3acc7e43398ad56a3ef95ecc03cdaf
pwalton134 <35519675+pwalton134@users.noreply.github.com>
2020-03-02 09:13:01 +0800

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -205,6 +205,21 @@ echo "---Server ready---"
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/games/openttd -D ${GAME_PARAMS}
+
+if [ "$GAME_PARAMS" = "last-game" ]; then
+	savegame=${SERVER_DIR}/openttd/save/autosave/`ls -rt ${SERVER_DIR}/openttd/save/autosave/ | tail -n1`
+		if [ -r ${savegame} ]; then
+	        echo "Last-game option specified.  Attempting to load now..."
+			echo "Loading ${savegame}"
+            screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/games/openttd -D -g ${savegame} -x
+        else
+			echo "${savegame} not found..."
+			screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/games/openttd -D
+		fi
+else
+	echo "Load last game not specified (use last-game parameter to invoke)"
+	screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/games/openttd -D ${GAME_PARAMS}
+fi
+
 sleep 2
 tail -f ${SERVER_DIR}/masterLog.0

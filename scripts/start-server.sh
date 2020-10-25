45e33bfc28ab54328c6edf24a455a5f47fa04778
pwalton134 <35519675+pwalton134@users.noreply.github.com>
2020-10-25 08:57:05 +0800

    Update start-server.sh
    Added
    ${SERVER_IP}:${GAME_PORT} and ${GAME_PARAMS} to each load case (lines: 19, 26, 29, 34)

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -16,22 +16,22 @@ if [ "$GAME_PARAMS" = "last-game" ]; then
         if [ -z "$(ls -A ${SERVER_DIR}/.openttd/save/autosave)" ]; then
                 echo "Autosave directory found, but empty"
                 echo "Starting a new game instead..."
-                screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D
+                screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${SERVER_IP}:${GAME_PORT} ${GAME_PARAMS}
         else
                 savegame=${SERVER_DIR}/.openttd/save/autosave/`ls -rt ${SERVER_DIR}/.openttd/save/autosave/ | tail -n1`
                 if [ -r ${savegame} ]; then
                         echo "Last-game option specified.  Attempting to load now..."
                         echo "Loading ${savegame}"
                         #screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/openttd/openttd -D -g ${savegame} -x
-                        screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D -g ${savegame} -x
+                        screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${SERVER_IP}:${GAME_PORT} -g ${savegame} -x ${GAME_PARAMS}
                 else
                         echo "${savegame} not found..."
-                        screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${GAME_PARAMS}
+                        screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${SERVER_IP}:${GAME_PORT} ${GAME_PARAMS}
                 fi
         fi
 else
         echo "Load last game not specified (use last-game parameter to invoke)"
-        screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${GAME_PARAMS}
+        screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${SERVER_IP}:${GAME_PORT} ${GAME_PARAMS}
 fi
 
 sleep 2

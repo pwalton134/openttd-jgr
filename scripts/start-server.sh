7e8c74db660492846ccdfaf53843b25661210393
pwalton134 <35519675+pwalton134@users.noreply.github.com>
2020-10-25 09:33:28 +0800

    Update start-server.sh
    Removed
    $GAME_PARAMS from all cases under if = "latest-game" (facepalm)

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -16,17 +16,18 @@ if [ "$GAME_PARAMS" = "last-game" ]; then
         if [ -z "$(ls -A ${SERVER_DIR}/.openttd/save/autosave)" ]; then
                 echo "Autosave directory found, but empty"
                 echo "Starting a new game instead..."
-                screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${SERVER_IP}:${GAME_PORT} ${GAME_PARAMS}
+                screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${SERVER_IP}:${GAME_PORT}
         else
                 savegame=${SERVER_DIR}/.openttd/save/autosave/`ls -rt ${SERVER_DIR}/.openttd/save/autosave/ | tail -n1`
                 if [ -r ${savegame} ]; then
                         echo "Last-game option specified.  Attempting to load now..."
                         echo "Loading ${savegame}"
                         #screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/openttd/openttd -D -g ${savegame} -x
-                        screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${SERVER_IP}:${GAME_PORT} -g ${savegame} -x ${GAME_PARAMS}
+                        screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${SERVER_IP}:${GAME_PORT} -g ${savegame} -x
+                        
                 else
                         echo "${savegame} not found..."
-                        screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${SERVER_IP}:${GAME_PORT} ${GAME_PARAMS}
+                        screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m openttd -D ${SERVER_IP}:${GAME_PORT}
                 fi
         fi
 else
